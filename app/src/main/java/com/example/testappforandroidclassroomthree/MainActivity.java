de0002c973e6d2338aec31d2f6dcf14f5f28d9e2
Nuke <InformaticNuke@gmail.com>
2023-09-28 20:49:14 -0300

    v0.002

--- app/src/main/java/com/example/testappforandroidclassroomthree/MainActivity.java
+++ app/src/main/java/com/example/testappforandroidclassroomthree/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.testappforandroidclassroomthree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtnOne, edtnTwo;
+    Spinner spnspinnerOne;
+    ListView lstlistOne;
+    List<String> results;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtnOne = findViewById(R.id.nOne);
+        edtnTwo = findViewById(R.id.nTwo);
     }
 }
